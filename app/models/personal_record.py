af3e96079bd38a54cb5aaac190a4c70e3034790c
mbosque9 <mbosque.9@gmail.com>
2021-05-13 19:52:38 +0200

    prova branca

--- app/models/personal_record.py
+++ app/models/personal_record.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from pydantic import BaseModel, Field
 from app.models.pyobject import PyObjectId
 
@@ -5,4 +6,16 @@ from app.models.pyobject import PyObjectId
 # fitxa tècnica
 class PersonalRecord(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    description: str = Field(...)
+    Risc: str = Field(...)
+
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "Risc": "Alt"
+
+            }
+        }
