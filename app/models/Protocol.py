10ff85f791fafa4b5ffa55951ffb27358a50cb79
mbosque9 <mbosque.9@gmail.com>
2021-05-17 16:24:54 +0200

    .

---
+++ app/models/Protocol.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from bson import ObjectId
+from pydantic import BaseModel, Field
+
+from app.models.pyobject import PyObjectId
+from app.models.Etapa import Etapa
+
+
+class Protocol(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    Etapes: List[Etapa] = None
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+
+            }
+        }
